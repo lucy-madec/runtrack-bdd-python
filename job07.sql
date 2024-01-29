b54ac6179a6bcafffff8fdf387d054c24ddfc18b
lucy-madec <144667458+lucy-madec@users.noreply.github.com>
2024-01-29 15:34:57 +0100

    job07 terminé

---
+++ job07.sql
@@ -0,0 +1,4 @@
+-- Ouverture du Terminal cmd en tant qu'administrateur.
+-- cd C:\Program Files\MySQL\MySQL Server 8.0\bin
+-- mysqldump -u root -p LaPlateforme > C:\Users\laplateforme.sql
+-- Entrée du mot de passe et ajout du fichier laplateforme.sql dans le VS Studio Code
