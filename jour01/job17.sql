a9063c22564db5572301d694118947fb75586dcc
lucy-madec <144667458+lucy-madec@users.noreply.github.com>
2024-01-29 17:57:57 +0100

    job17 terminé

---
+++ jour01/job17.sql
@@ -0,0 +1,2 @@
+UPDATE etudiant SET age = 20 WHERE nom = 'Spaghetti' AND prenom = 'Betty'; -- Changement de l'âge.
+SELECT * FROM etudiant WHERE nom = 'Spaghetti' AND prenom = 'Betty'; -- Vérification du changement pour Betty.
