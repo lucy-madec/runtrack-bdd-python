964df158520630ce441cfe891fa4103be05097a0
lucy-madec <144667458+lucy-madec@users.noreply.github.com>
2024-01-29 18:02:02 +0100

    job18 terminé

---
+++ jour01/job18.sql
@@ -0,0 +1,2 @@
+DELETE FROM etudiant WHERE nom = 'Doe' AND prenom = 'John'; -- Suppression de l'étudiant John Doe
+SELECT FROM etudiant; -- Vérification que le nom n'est plus dans la liste de la table etudiant.
