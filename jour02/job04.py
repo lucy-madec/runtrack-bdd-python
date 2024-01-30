485cc4e4b8a6aee6be6957906e52a3552717d3ba
lucy-madec <144667458+lucy-madec@users.noreply.github.com>
2024-01-30 12:15:02 +0100

    job04 terminé

---
+++ jour02/job04.py
@@ -0,0 +1,24 @@
+import mysql.connector
+
+# Connexion à la base de données
+conn = mysql.connector.connect(
+    host="localhost",
+    user="root",
+    password="&Dance13008",
+    database="LaPlateforme"
+)
+
+# Création d'un objet curseur
+cursor = conn.cursor()
+
+# Exécution de la requête pour récupérer l'ensemble des étudiants
+query = "SELECT nom, capacite FROM salle"
+cursor.execute(query)
+
+# Affichage du résultat dans le terminal
+print("Noms et capacités des salles :")
+for row in cursor.fetchall():
+    print(f"Nom: {row[0]}, Capacité: {row[1]}")
+
+cursor.close()
+conn.close()
