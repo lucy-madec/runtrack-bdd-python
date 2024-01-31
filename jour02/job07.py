29b5463d15b21fb2eba3e7e5f5059bb7a0289072
lucy-madec <144667458+lucy-madec@users.noreply.github.com>
2024-01-31 08:35:05 +0100

    Ajout du code MySQL en commentaire

--- jour02/job07.py
+++ jour02/job07.py
@@ -1,3 +1,40 @@
+# Création de la nouvelle base de données
+# CREATE DATABASE ma_base_de_donnees;
+# Sélection de la nouvelle base de données
+# USE ma_base_de_donnees;
+# -- Création de la table "employe"
+# CREATE TABLE employe (
+#     id INT PRIMARY KEY AUTO_INCREMENT,
+#     nom VARCHAR(255),
+#     prenom VARCHAR(255),
+#     salaire DECIMAL(10, 2),
+#     id_service INT
+# );
+# -- Insérer des employés dans la table "employe"
+# INSERT INTO employe (nom, prenom, salaire, id_service) VALUES ('Lamorte', 'Vanny', 3500.00, 1);
+# INSERT INTO employe (nom, prenom, salaire, id_service) VALUES ('Lorquet', 'Ines', 4000.00, 2);
+# INSERT INTO employe (nom, prenom, salaire, id_service) VALUES ('Iribaren', 'Lucas', 3000.00, 3);
+# INSERT INTO employe (nom, prenom, salaire, id_service) VALUES ('Martinie', 'Lucas', 2800.00, 4);
+# -- Récupérer les employés avec un salaire supérieur à 3 000 €
+# SELECT * FROM employe WHERE salaire > 3000;
+
+# -- Ajouter la table "service"
+# CREATE TABLE service (
+#     id INT PRIMARY KEY AUTO_INCREMENT,
+#     nom VARCHAR(255)
+# );
+
+# -- Insérer des services dans la table "service"
+# INSERT INTO service (nom) VALUES ('Dev Web');
+# INSERT INTO service (nom) VALUES ('IA');
+# INSERT INTO service (nom) VALUES ('Cybersecurite');
+# INSERT INTO service (nom) VALUES ('Dev Logiciel');
+
+# -- Récupérer tous les employés et leur service respectif
+# SELECT e.id, e.nom, e.prenom, e.salaire, s.nom AS nom_service
+# FROM employe e
+# JOIN service s ON e.id_service = s.id;
+
 import mysql.connector
 
 class Employe:
